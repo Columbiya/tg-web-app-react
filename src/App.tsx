3ce89fb15fc7901dae0f6e10d57d4f78dbc11929
cosmofund-admin <admin@cosmofund.space>
2022-11-23 19:45:48 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+const tg = Telegram.WebApp
+
 function App() {
+
+  useEffect(() => {
+    tg.ready() //является хорошей практикой сообщает о том, что приложение полностью инициализировалось
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={onClose}>Зыкрыть</button>
     </div>
   );
 }
