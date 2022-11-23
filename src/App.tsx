5066ad4dab76e08760aca7440b66a2e8250879c3
cosmofund-admin <admin@cosmofund.space>
2022-11-23 20:11:28 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { Header } from './components/Header/Header';
+import { useTelegram } from './components/hooks/useTelegram';
 
 function App() {
+  const { tg } = useTelegram()
+  
+  useEffect(() => {
+    tg.ready() //является хорошей практикой сообщает о том, что приложение полностью инициализировалось
+  }, [])
 
   return (
     <div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,15 @@
 import { useEffect } from "react"
-import { tg } from "../../utils/tg"
 import { Button } from "../Button/Button"
+import { useTelegram } from "../hooks/useTelegram"
 
 
 export const Header: React.FC = () => {
-
-    useEffect(() => {
-        tg.ready() //является хорошей практикой сообщает о том, что приложение полностью инициализировалось
-      }, [])
+    const { onClose, tg, user } = useTelegram()
     
-    const onClose = () => {
-        tg.close()
-    }
-
     return (
         <header className="header">
             <Button onClick={onClose}>Закрыть</Button>
-            <span className="username">{tg.initDataUnsafe?.user?.usernames}</span>
+            <span className="username">{user?.usernames}</span>
         </header>
     )
 }
